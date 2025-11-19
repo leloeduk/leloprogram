f3784192413c1d1b75bd0d1aa16f451977b804c9
leloeduk <leloeduk2025@gmail.com>
2025-11-19 22:16:41 +0100

    Création des ViewSets pour tous les modèles

--- programs/views.py
+++ programs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Subject, Level, SchoolClass, TeachingProgram
+from .serializers import SubjectSerializer, LevelSerializer, SchoolClassSerializer, TeachingProgramSerializer
 
-# Create your views here.
+class SubjectViewSet(viewsets.ModelViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+class SchoolClassViewSet(viewsets.ModelViewSet):
+    queryset = SchoolClass.objects.all()
+    serializer_class = SchoolClassSerializer
+
+class TeachingProgramViewSet(viewsets.ModelViewSet):
+    queryset = TeachingProgram.objects.all()
+    serializer_class = TeachingProgramSerializer
